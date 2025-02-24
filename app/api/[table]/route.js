498dba110bfb2ceaf3015db25df9f0971869d1ba
David Cordero de Jesus <decorde@yahoo.com>
2025-02-23 20:32:26 -0600

    added posting

--- app/api/[table]/route.js
+++ app/api/[table]/route.js
@@ -1,6 +1,6 @@
 import db from '@/lib/db';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/dist/server/api-utils';
+import { redirect } from 'next/navigation';
 
 export async function GET(req) {
   const table = req.url.split('/').pop();
